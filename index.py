82be58eeebcb3d5ed28770cdb9c3a7409b85d9ef
Rubens Gomes <vitorr_gomes@hotmail.com>
2017-07-03 22:28:16 -0300

    remove logs and add comments

--- index.py
+++ index.py
@@ -7,11 +7,6 @@ import json
 import math
 
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-from scipy.stats import itemfreq
-from skimage.feature import hog , local_binary_pattern
 
 RESULTS = []
 
@@ -50,48 +45,55 @@ def load_dataset():
 	return dataset,writePath
 
 # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
+# In this method we apply many morphological operations and
+# filters to reduce noising of the image
+# because our dataset its very sparse and mixed
 def find_contours(image):
-	#erosion
+	# For the utilization of Otsu threshold its needed to apply a gaussian blur
+	# we use a big kernel to instensify the noising remove
+	blur = cv2.GaussianBlur(image,(11,11),0)
 	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
-	grad = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, kernel)
-	#binarize
+	grad = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, kernel)
+
+	# Applying Otsu binarization its very useful because
+	# he uses the advantage of a gaussian blured image
+	# and calculates an optimal threshold based on the variance and means
+	# wich is useful since we have many different images
+	# http://docs.opencv.org/trunk/d7/d4d/tutorial_py_thresholding.html
 	_, thresh = cv2.threshold(grad, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 	#dilation
+	# we dilate to remove some noise resulted of the transformation
+	dilated = cv2.dilate(thresh,kernel, iterations = 2) # dilate
+
 	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
-	# dilated = cv2.dilate(thresh,kernel, iterations = 3) # dilate
 	connected = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 	_, contours, hierarchy = cv2.findContours(connected,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
 	cv2.imshow('contours', connected)
-	cv2.imwrite('results/' + 'results_'+ 'teste' +'.jpg', connected)
 	cv2.waitKey(0)
 	return contours
 
 
+# apply the rectangle draw on the interesting regions
 def drawRectangle(original, contours):
 	for contour in contours:
 		# get rectangle bounding contour
 		[x, y, w, h] = cv2.boundingRect(contour)
 
-		#be smart and draw only in common signature regions
+		#being smart and draw only in commonly signature regions
 		valid = original.shape[1] * 0.50
 		# bootom of the figure
 		# horizontal text
 		if (w > h and y > valid and w > 20 and h > 20 ):
-		# draw rectangle around contour on original image
+		# draw rectangle around the contour on the original image
 			marked = cv2.rectangle(original, (x, y), (x + w, y + h), (255, 0, 255), 2)
 			return marked
-
-#Resize the image to do analysis easier
-def applyROI(image):
-	image = cv2.resize(image, (256, 256))
-	return image
 
 def process_images(dataset):
 	print("Start Processing")
 	for (i, imagePath) in enumerate(dataset):
-
+		# get the current gray image
 		image = cv2.imread(imagePath, 0)
-		# image = applyROI(image)
+		# Find all contours in the gray image
 		contours = find_contours(image)
 		# Get the image with rectangles and save
 		img_processed = drawRectangle(image, contours)
@@ -107,7 +109,6 @@ def process_images(dataset):
 def main(argv):
 	DATASET,WRITE_PATH = load_dataset()
 	process_images(DATASET)
-	# save_results(WRITE_PATH)
 
 if __name__ == "__main__":
     main(sys.argv)
